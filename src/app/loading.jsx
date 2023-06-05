293775fa4ed5e5426f375a92a0da9aaabe753be5
Demyus <orlando.agundez.ramirez@gmail.comm>
2023-06-04 18:41:27 -0700

    adding components

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+
+export default function loading() {
+    return (
+        <div className="flex justify-center">
+            <img className="h-96" src="spinner.svg" alt="Loading..." />
+        </div>
+        
+    )
+}

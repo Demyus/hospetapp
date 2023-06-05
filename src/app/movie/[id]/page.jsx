1baa0d25a6482165dd061b9656cbf9c82717eb1a
Demyus <orlando.agundez.ramirez@gmail.comm>
2023-06-04 21:49:19 -0700

    creating listing

---
+++ src/app/movie/[id]/page.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+async function getMovie(movieID) {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.API_KEY}`);
+    return await res.json();
+}
+
+export default async function MoviePage({params}) {
+    const movieID = params.id;
+    const movie = await getMovie(movieID);
+
+    return ( 
+        <div className="w-full">
+            <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6" >
+              <Image 
+                    src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} width={500} height={300} className="rounded-lg" 
+                    placeholder="blur" 
+                    blurDataURL="/spinner.svg" 
+                    alt="Movie Poster"
+                    style={{
+                        maxWidth: '100%',
+                        height: '100%'
+                    }}
+                />
+                <div className="p-2">
+                    <h1 className="text-lg mb-3 font-bold">{movie.title || movie.name}</h1>
+                    <p className="text-lg mb-3">
+                        <span className="font-semibold mr-1">Overview:</span>
+                        {movie.overview}
+                    </p>
+                    <p className="mb-3">
+                        <span className="font-semibold mr-1">Date Released:</span>
+                        {movie.release_date || movie.first_air_date}
+                    </p>
+                    <p className="mb-3">
+                        <span className="font-semibold mr-1">Rating:</span>
+                        {movie.vote_count}
+                    </p>
+                </div>
+            </div>
+
+            
+        </div>
+    )
+}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,11 @@
+import Card from '@/components/Card'
 
 const Results = ({results}) => {
     return (
-        <div>
-            {results.map((result) => (
-                <div key={result.id}>
-                    {result.original_title}
-                </div>
-            ))}
+        <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-6xl mx-auto py-4'>
+            { results.map((result) => (
+                <Card key={result.id} result={result} />
+            )) }
         </div>
     )
 }

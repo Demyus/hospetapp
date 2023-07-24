1fcfec92b551f2a43347ad0bad274f7f254dd773
Demyus <orlando.agundez.ramirez@gmail.comm>
2023-07-23 21:58:19 -0700

    quickie for deployment tests

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,10 +11,10 @@ import React from 'react';
 const manrope = Manrope({ subsets: ['latin'] })
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Hospetapp',
-  description: 'Test App',
-}
+// export const metadata = {
+//   title: 'Hospetapp',
+//   description: 'Test App',
+// }
 
 export default function RootLayout({ children }) {
 
